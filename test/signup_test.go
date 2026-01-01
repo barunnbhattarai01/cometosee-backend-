1edcbe644adc0539fb37687106bdd1ca4f00f566
barunnbhattarai01 <barunnbhattarai@gmail.com>
2026-01-01 10:53:40 +0545

    feat: signup tester added for better debugging

---
+++ test/signup_test.go
@@ -0,0 +1,52 @@
+package test
+
+import (
+	"cometosee/controller"
+	"cometosee/intailizer"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestSignup(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Fatalf("Error loading env file: %v", err)
+	}
+	intailizer.DatabaseConnection()
+
+	if intailizer.DB == nil {
+		t.Fatalf("DB not initialized")
+	}
+
+	email := "testing@gmail.com"
+	body := `{
+	 "username":"tester",
+	 "email":"` + email + `",
+	 "password":"password123"
+	}`
+
+	req := httptest.NewRequest("POST", "/signup", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	controller.Signup(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf("Expected 200, got %d", rec.Code)
+	}
+
+	var count int
+	err = intailizer.DB.QueryRow(
+		`SELECT COUNT(*) FROM cometoseeauth WHERE email=$1`, email,
+	).Scan(&count)
+
+	if err != nil || count != 1 {
+		t.Fatalf("User not inserted into database")
+	}
+
+	defer intailizer.DB.Exec(`DELETE FROM cometoseeauth WHERE email=$1`, email)
+}
